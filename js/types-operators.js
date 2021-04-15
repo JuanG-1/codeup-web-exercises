ff1910086b9618d8ce059d13c57bfb412ba9e0b9
Juang-1 <j.gonzalezmarroquin@gmail.com>
2021-04-15 10:23:27 -0500

    Push

---
+++ js/types-operators.js
@@ -0,0 +1,51 @@
+"use strict";
+
+var isTrue = true; // boolean value
+
+console.log("The value of isTrue;" + isTrue); // boolean literal
+
+//----------------------BOOLEAN
+
+var iAmHungry = true;
+
+if (iAmHungry){
+    console.log("Let's find some food fam!")
+}
+
+var hoursSinceMeal = 5;
+
+var iAmHungry = (hoursSinceMeal >4);
+
+if (hoursSinceMeal >4) { // Boolean exercise//
+    console.log("I'm getting a little hungry");
+}
+
+//------------------------------------------NUMBERS
+
+console.log("The number is:" + 10)
+console.log("The number is: " + 198467)
+console.log("The number is: " +-1754)
+
+console.log(10 + 10); // addition operator
+
+console.log(2-8);    // subtraction operator
+
+console.log(4 * 4); // multiplication operator
+
+console.log(1 / 0); // division operator
+
+console.log(12 % 3); // modulo operator/divides and answer will be the remainder
+
+// Mini Exercise - Numbers and Math
+// TODO: console.log() five equations using each arithmetic operator (+,-,*,/,%) which EACH will equal 4
+
+console.log(3 + 1);
+
+console.log(5 - 1);
+
+console.log(2 * 2);
+
+console.log(4 / 1);
+
+console.log(4 % 28);
+
