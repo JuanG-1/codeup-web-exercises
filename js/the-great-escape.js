d1c22e94cb203242f6a75ce0d1e4abc38471b284
Juang-1 <j.gonzalezmarroquin@gmail.com>
2021-04-16 13:50:16 -0500

    Push

--- js/the-great-escape.js
+++ js/the-great-escape.js
@@ -1,24 +1,51 @@
-function(){
-    "use strict";
-}
+(function(){
+        "use strict";
+        function getKindOfDog(){
+            return prompt("What kind of dog is it?");
+        }
 
+        function getContactInfo(){
+            return prompt("How do I contact you when I find the dog?");
+        }
 
-function informWhatKindOfDog(){
-    prompt("What kind of dog is it?")
-}
+        function getDogName(){
+            return prompt("What is your dog's name?");
+        }
 
-console.log(informWhatKindOfDog());
+        function playTheGreatEscape(){
 
-function getContactInfo(){
-    return prompt( "How do I contact you when I find the dog?");
+            //get all the info
+            var dogKind = getKindOfDog();
+            var dogName = getDogName();
+            var contactInfo = getContactInfo();
 
-}
+            //start searching
+            alert("Time to search for this doggo!");
 
-console.log(getContactInfo())
+            searchForDog(dogName, dogKind, contactInfo);
 
-function getDogName(){
-    prompt("What does your dog eat?");
+        }
 
-}
+        function searchForDog(dogName, dogKind, contactInfo){
+            //see a dog!
+            alert("I see a dog!");
+            var kindOfDogSeen = getKindOfDog();
 
+            //check if the dog matches
+            var isTheLostDog = (kindOfDogSeen === dogKind) && confirm("Does dog respond to the name: " + dogName);
+
+            if (isTheLostDog){
+                //return dog to the owner!
+                alert("Puts leash on " + dogName);
+                alert("Time to call the owner!");
+                alert("Calling: " + contactInfo);
+                alert("Taking the dog to their worried owner!");
+            }else{
+                alert("That's not the right dog, keep searching!");
+                searchForDog(dogName, dogKind, contactInfo);
+            }
+        }
+        playTheGreatEscape();
+    }
+)()
 
