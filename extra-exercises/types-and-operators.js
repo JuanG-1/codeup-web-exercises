54265ab7378f370ec4623c0937639f3823d55e7f
Juang-1 <j.gonzalezmarroquin@gmail.com>
2021-04-29 07:39:20 -0500

    more code

---
+++ extra-exercises/types-and-operators.js
@@ -0,0 +1,81 @@
+(function () {
+    "use strict";
+
+//     function isOdd(input){
+//         return input % 2 !== 0;
+//     }
+//
+//     function isEven(input){
+//         return input % 2 === 0;
+//     }
+//
+//     function isTrue(input){
+//         return input === true;
+// }
+//
+//
+//     function isFalse(input){
+//         return input === false;
+//     }
+//
+//
+//
+// function subtract(a, b){
+//     if((typeof a === "number" || !isNaN(a))
+//         && (typeof b === "number" || !isNaN(b))  ){
+//         return a - b;
+//     }else{
+//         return false;
+//     }
+//
+// }
+//
+//     console.log(subtract(3, 2))
+//
+// function multiply(a, b){
+//     return a * b;
+// }
+//
+// function remainder(a, b){
+//     return a / b;
+//
+// }
+//
+// function multiplyIfNumeric(input1, input2){
+//         var product = Number(input1) * Number(input2)
+//
+//     if (isNaN(product)) {
+//         return false
+//     }else{
+//         return product;
+//     }
+// }
+//
+//     console.log(multiplyIfNumeric(5,5))
+//
+//     function toUpperCase(input){
+//         if (typeof input === "string" && isNaN(input)) {
+//             return input.toUpperCase()
+//         }
+//             return false
+//     }
+//
+//     console.log(toUpperCase("undefined"))
+//
+
+// TODO: write a function called stickyCaps which takes in an input and returns that input in sticky caps
+//  ie: dog -> dOg, software developer -> sOfTwArE dEvElOpEr
+//  -> You may be using a few different string methods
+//  -> If the input is numeric or not a string, return false
+
+    function stickyArr(input){
+        return input
+    }
+
+    console.log(stickyArr("juan"))
+
+
+
+
+})();
+
