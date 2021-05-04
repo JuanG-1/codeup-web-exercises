6ed918e1b1086af463a6f15216160c61a0b7183d
Juang-1 <j.gonzalezmarroquin@gmail.com>
2021-05-04 11:32:30 -0500

    forms exercises

--- JS/Monday Warmup.js
+++ JS/Monday Warmup.js
@@ -5,4 +5,21 @@ function trim(input){
 }
 
 console.log(trim("string"))
+function trim(input){
+    return input.replaceAll("", " ")
+}
 
+// Write a function named cube that returns n * n * n
+// Write a function named cube Root that returns the cube root of the input
+
+function cube(num){
+    return num * num * num
+}
+
+console.log(cube(3))
+
+function cubeRoot(num){
+    return Math.cbrt(num);
+}
+
+console.log(cubeRoot(27))
