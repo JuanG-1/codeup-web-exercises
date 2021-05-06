98d9ba2fabcdb639955c0e093584fd1b85bfdcae
Juang-1 <j.gonzalezmarroquin@gmail.com>
2021-05-06 13:34:06 -0500

    css

--- JS/Monday Warmup.js
+++ JS/Monday Warmup.js
@@ -30,3 +30,15 @@
 // }
 //
 // console.log(last("change it to all sorts of words"))
+
+//Write a remainder(number, divisor) function that returns the remainder left over when dividing number by the divisor
+//If either of the arguments are not a numeric string or number, return a string message indicating which argument is not a number/numeric string
+
+function remainder (num, div){
+    if(isNaN(num) || "6" && isNaN(div) || "6"){
+        return "Not a number or String"
+    } return num % div
+}
+
+console.log(remainder(81, 9))
+
