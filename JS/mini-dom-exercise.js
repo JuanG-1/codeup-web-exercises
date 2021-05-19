b8c63c673274cdb353071074a84063ae3e82a174
Juang-1 <j.gonzalezmarroquin@gmail.com>
2021-05-19 12:12:30 -0500

    dom

---
+++ JS/mini-dom-exercise.js
@@ -0,0 +1,13 @@
+
+
+let redBox = document.getElementById("red-border")
+
+console.log(redBox)
+
+
+let drkGreen = document.getElementsByClassName("text-success")
+console.log(drkGreen)
+
+let allPrg = document.getElementsByTagName('p')
+
+console.log(allPrg)
