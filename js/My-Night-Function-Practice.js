350f5952905d6b4a8e9fa9b341e2f641a435de19
Juang-1 <j.gonzalezmarroquin@gmail.com>
2021-05-22 22:41:47 -0500

    101-exercises and Numify function

--- js/My-Night-Function-Practice.js
+++ js/My-Night-Function-Practice.js
@@ -1,28 +1,34 @@
-
-
-var numify  = isNaN && null && true && false && typeof "string" && "5"
-
-var p4 = 357 * 9
-console.log("this is:" + p4)
-var p3 = 2 * 5;
-console.log("this is: " + p3)
-function practice(p1,p2){
-    if(p1 === numify && p2 === numify ){
-return "that is not a number"
-    }
-return p3 * p1 * p2 + p4;
-}
-
-console.log(practice("9",5,))
-
-
-
-//Christians Numify Function///
-
+//
+//
+// var numify  = isNaN && null && true && false && typeof "string" && "5"
+//
+// var p4 = 357 * 9
+// console.log("this is:" + p4)
+// var p3 = 2 * 5;
+// console.log("this is: " + p3)
+// function practice(p1,p2){
+//     if(p1 === numify && p2 === numify ){
+// return "that is not a number"
+//     }
+// return p3 * p1 * p2 + p4;
+// }
+//
+// console.log(practice("9",5,))
+//
+//
+//
+// //Christians Numify Function///
+//
 function numify(num){
     console.log(num);
     if(typeof num === "string" || typeof num === "number")
         return Number(num)
     return NaN;
 }
+
+
+var names = ["bill", "josh", "james", 1, 2, 3, false]
+
+
+
 
---
+++ js/dom-events.js
@@ -0,0 +1,53 @@
+
+
+
+
+addButtonEvent()
+function addButtonEvent() {
+    document.getElementById("submitBtn")
+        .addEventListener("click", addPost)
+}
+
+
+function addPost() {
+    var container = document.getElementById("blog-container")
+    var newPost = document.createElement("p")
+    var blogInput = document.getElementById("user-input")
+    container.appendChild(newPost)
+    newPost.innerText = blogInput.value
+}
+
+addSecondButton()
+function addSecondButton(){
+    document.getElementById("submitBtn2").addEventListener("click", addSecondPost)
+
+}
+
+function addSecondPost(){
+
+    var container2 = document.getElementById("blog-container2")
+    var newPost2 = document.createElement("p")
+    var blogInput2 = document.getElementById("user-input2")
+    container2.appendChild(newPost2)
+    newPost2.innerText = blogInput2.value
+}
+
+
+
+
+
+
+
+
+
+// function addPost() {
+//     document.getElementById("blog-container").appendChild(BlogContent());
+//
+// }
+//
+// function BlogContent() {
+//     let p = document.createElement("p");
+// p.textContent = "random text"
+//     document.getElementById("userInput").innerHTML
+//     return p;
+// }
