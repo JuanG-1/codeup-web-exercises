b21bf9a674d18d6989a3cdb81d8c6c80e35f38dd
Juang-1 <j.gonzalezmarroquin@gmail.com>
2021-05-30 12:10:15 -0500

    css

--- js/conditionals-exercises.js
+++ js/conditionals-exercises.js
@@ -21,9 +21,9 @@
 
     //region TODO: IF/ELSE
 
-    As we talked about previously,
-    programming, electrical engineering, and most basic human decisions revolve around
-    deciding to do one thing or another based on some input
+    // As we talked about previously,
+    // programming, electrical engineering, and most basic human decisions revolve around
+    // deciding to do one thing or another based on some input
 
     var hasCar = true;
 
@@ -32,8 +32,8 @@
     } else{
         console.log("We need to call a rideshare!");
     }
-
-    expanding on this idea, let's recall boolean expressions
+    //
+    // expanding on this idea, let's recall boolean expressions
 
     var age = 22;
     var isLicensed = age >= 16;
@@ -46,9 +46,9 @@
         console.log("We need to call a rideshare!");
     }
 
-    Let's bring back in functions!
-    I see some variables in this function that can be extracted
-    This will make our function reusable!
+    // Let's bring back in functions!
+    // I see some variables in this function that can be extracted
+    // This will make our function reusable!
 
 
     function getToDestination(age, isInsured, hasCar){
@@ -64,16 +64,16 @@
     }
 
     getToDestination(22, true, true);
-    endregion
-
-    We just painted a beautiful little function right there
-
-    region TODO: IF / ELSE IF / ELSE
-
-
-    Sometimes we have more than just two scenarios that need to be evaluated
-
-    Let's say there are no rideshares nearby. Let's find another mode of transportation
+    // endregion
+    //
+    // We just painted a beautiful little function right there
+    //
+    // region TODO: IF / ELSE IF / ELSE
+    //
+    //
+    // Sometimes we have more than just two scenarios that need to be evaluated
+    //
+    // Let's say there are no rideshares nearby. Let's find another mode of transportation
 
     function getToDestination(age, isInsured, hasCar, canGetRideshare){
         var isLicensed = age >= 16;
@@ -90,23 +90,23 @@
 
     getToDestination(22, true, false, true);
 
-    endregion
-
-    TODO: TO NOTE: You do not need an else statement for the conditional expression to be valid
-     -> However, *be sure* that you have some sort of fallback logic to handle unforeseen conditions
-
-    region TODO: MINI EXERCISE -> IF / ELSE IF / ELSE
-    TODO: MINI EXERCISE -> IF / ELSE IF / ELSE
-     -> Let's augment our getToDestination() function
-     -> Create a function called canGetRideshare() which will *RETURN* a boolean
-         -> It will determine if there are drivers nearby and the person has enough money for the ride
-     -> Now, when you call getToDestination(), instead of passing in a canGetRideShare parameter
-     -> Call canGetRideshare() in the else if *after* !isLegalDriver &&...
-     -> Be sure to log to the user if they can or cannot get a rideshare
-    TODO: BONUS -> This exercise will probably get the mind wondering "How can I make this more?.."
-     -> Consider this: What if mileage determined:
-         -> If the rider has enough funds?
-         -> If there is a driver close enough to service the rider?
+    // endregion
+    //
+    // TODO: TO NOTE: You do not need an else statement for the conditional expression to be valid
+    //  -> However, *be sure* that you have some sort of fallback logic to handle unforeseen conditions
+    //
+    // region TODO: MINI EXERCISE -> IF / ELSE IF / ELSE
+    // TODO: MINI EXERCISE -> IF / ELSE IF / ELSE
+    //  -> Let's augment our getToDestination() function
+    //  -> Create a function called canGetRideshare() which will *RETURN* a boolean
+    //      -> It will determine if there are drivers nearby and the person has enough money for the ride
+    //  -> Now, when you call getToDestination(), instead of passing in a canGetRideShare parameter
+    //  -> Call canGetRideshare() in the else if *after* !isLegalDriver &&...
+    //  -> Be sure to log to the user if they can or cannot get a rideshare
+    // TODO: BONUS -> This exercise will probably get the mind wondering "How can I make this more?.."
+    //  -> Consider this: What if mileage determined:
+    //      -> If the rider has enough funds?
+    //      -> If there is a driver close enough to service the rider?
 
     function canGetRideshare(){
 
@@ -123,16 +123,16 @@
 
         return getRideshare;
     }
-    endregion
-
-    region TODO: TERNARY STATEMENTS
-
-    TODO: TO NOTE: Ternary Statements should be used carefully and purposefully
-     -> Best Practice is to use them in order to RETURN a value to something else
-     -> Another best practice: If you have more than one condition, don't use it
-         -> Anything more will make the code difficult to read
-
-    TODO: Without Ternary
+    // endregion
+    //
+    // region TODO: TERNARY STATEMENTS
+    //
+    // TODO: TO NOTE: Ternary Statements should be used carefully and purposefully
+    //  -> Best Practice is to use them in order to RETURN a value to something else
+    //  -> Another best practice: If you have more than one condition, don't use it
+    //      -> Anything more will make the code difficult to read
+    //
+    // TODO: Without Ternary
     function showFatType(){
         var fatType = "margarine";
         var isButter = fatType.toLowerCase() === "butter";
@@ -147,7 +147,7 @@
         console.log(loggingStatement);
     }
 
-    TODO: With Ternary
+    // TODO: With Ternary
     function showFatType(){
         var fatType = "margarine";
         var isButter = fatType.toLowerCase() === "butter";
@@ -159,7 +159,7 @@
         console.log(loggingStatement);
     }
 
-    TODO: Bad Ternary
+    // TODO: Bad Ternary
     function showFatType(){
 
         var fatType = "margarine";
@@ -176,23 +176,23 @@
         console.log(loggingStatement);
     }
 
-    DEEP DIVE: https://stackoverflow.com/questions/10526739/a-somewhat-painful-triple-nested-ternary-operator
-
-    endregion
-
-    region TODO: SWITCH STATEMENTS
-
-    Sometimes, we have one variable that could have multiple values
-    Each different value could trigger a different response or return
-    In this case, a SWITCH STATEMENT could be the tool to use!
-
-    Let's say we need to know someone's age in order to determine
-    their ideal heart rate range
-
-    If we wrote an if/if else, we would have numerous ifs!
-    age >= 18 && age < 25, age >= 25 && age < 35, etc, etc
-
-    Instead, this is a great case for a switch statement!
+    // DEEP DIVE: https://stackoverflow.com/questions/10526739/a-somewhat-painful-triple-nested-ternary-operator
+    //
+    // endregion
+    //
+    // region TODO: SWITCH STATEMENTS
+    //
+    // Sometimes, we have one variable that could have multiple values
+    // Each different value could trigger a different response or return
+    // In this case, a SWITCH STATEMENT could be the tool to use!
+    //
+    // Let's say we need to know someone's age in order to determine
+    // their ideal heart rate range
+    //
+    // If we wrote an if/if else, we would have numerous ifs!
+    // age >= 18 && age < 25, age >= 25 && age < 35, etc, etc
+    //
+    // Instead, this is a great case for a switch statement!
 
 
     function getHeartRateRange(){
@@ -260,6 +260,8 @@
     //      -> HINT: a way you can check for errors is to see if the string contains the word "error"
     //  -> After all switch statement functions successfully return the user's input, concatenate them all into a nicely-formatted string and return all data to the user
     //endregion
+
+
 
 })()
 
