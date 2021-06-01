bfa0388445b75cfcddef931bdd2a4d3aa424ae7c
Juang-1 <j.gonzalezmarroquin@gmail.com>
2021-06-01 10:52:42 -0500

    jquery-lecture

---
+++ js/jquery-lecture.js
@@ -0,0 +1,32 @@
+//jquery selectors
+//
+// $("h1"); //changing by tag name
+//
+// $("#change-text"); //id name changes
+//
+// $(".lead"); // by class names
+//
+//
+// //how to hide elements
+// $("p").hide();
+//
+// ///hide a class by id
+// $("#change-text").hide();
+//
+// //hiding by class
+// $(".purple").hide();
+//
+// //show class once again
+// $(".purple").show();
+
+
+//@channel TODO: Using jQuery ... 1. hide the paragraph that is in all caps
+// 2.hide all the spans 3. show all the elements that have a ‘show-me’ class.
+// refresh your page between every task. d
+
+$("#all-caps").hide();
+
+$("span").hide()
+
+$(".show-me").show();
+
