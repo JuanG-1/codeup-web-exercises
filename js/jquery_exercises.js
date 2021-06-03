350b8eadcd74347c7ad7b91fe17a419ce2767445
Juang-1 <j.gonzalezmarroquin@gmail.com>
2021-06-03 14:44:10 -0500

    jquey events changing text from japanese to english

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,5 +1,55 @@
-$("#p-tag").css("color", "red",)
-$(".lists").css("font-size", "50px")
+$('#header').hover(
+    function() {
+        $(this).css('background-color', 'red');
+    },
+    function() {
+        $(this).css('background-color', 'blue');
+    }
+);
 
-$("div, p, li, ul ").hide()
-$("div, p, li, ul ").show()
+
+$("#p-tag").dblclick(
+    function(){
+        $(this).css("font-size", "18px")
+    }
+);
+
+$('li').hover(
+    function() {
+        $(this).css('color', 'red');
+    },
+    function() {
+        $(this).css('color', 'black');
+    }
+);
+
+
+
+$(document).ready(function () {
+    $("#header").hover(function() {
+        var $self = $(this);
+        var originalText = $self.text().trim();
+
+        $("#header").text("インターポロエーターの時間");
+
+        if (originalText == "インターポロエーターの時間") {
+            $self.text("Time for the Interpolator");
+        }
+
+    });
+});
+
+
+$(document).ready(function () {
+    $("#list1").hover(function() {
+        var $self = $(this);
+        var originalText = $self.text().trim();
+
+        $("#list1").text("ランダム1");
+
+        if (originalText == "ランダム1") {
+            $self.text("random 1");
+        }
+
+    });
+});
