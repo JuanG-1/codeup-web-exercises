6553a8d355b007ea47575a0a6a7f38573683a5cb
Juang-1 <j.gonzalezmarroquin@gmail.com>
2021-06-10 09:57:50 -0500

    thursday

--- js/mapbox-playground.js
+++ js/mapbox-playground.js
@@ -2,6 +2,7 @@ const mapboxToken = "pk.eyJ1Ijoic29uZXJvODQiLCJhIjoiY2twcG44ODlpMDY0aTJ3bW9vbHk2
 
 mapboxgl.accessToken = mapboxToken;
 
+
 var map = new mapboxgl.Map({
     container: "map",
     style: "mapbox://styles/mapbox/satellite-v9",
@@ -22,24 +23,24 @@ addGeocoderEvent(geocoder);
 setPopup(geocoder);
 
 //creates a returns a new geocoder
-function setGeocoder(){
+function setGeocoder() {
     return new MapboxGeocoder({
-accessToken: mapboxgl.accessToken,
+        accessToken: mapboxgl.accessToken,
         mapboxgl: mapboxgl,
         marker: false
     })
 }
 
 ///adds geocoder to map
-function addGeocoderToMap(geocoder){
+function addGeocoderToMap(geocoder) {
     map.addControl(geocoder);
 }
-///adds event listener to geocoder and sets new marker location
-function addGeocoderEvent(geocoder){
-    geocoder.on("result", function(event){
-        marker.setLngLat(event.result.geometry.coordinates)
 
-        setPopup(event.result.resultPlaceName)
+///adds event listener to geocoder and sets new marker location
+function addGeocoderEvent(geocoder) {
+    geocoder.on("result", function (event) {
+        marker.setLngLat(event.result.geometry.coordinates)
+        setPopup(event.result.place_name)
     })
 }
 
@@ -49,17 +50,32 @@ function setMarker(point) {
     return new mapboxgl.Marker().setLngLat(point)
         .addTo(map);
 }
+
 ///adds event to ,ap that changes location of marker based on where the user clicks
-function addMapEvent(){
-    map.on("click", function(event){
+function addMapEvent() {
+    map.on("click", function (event) {
         marker.setLngLat(event.lngLat)
             .addTo(map)
     })
 }
 
-function setPopup(textDetails){
+///creates a popup event
+function setPopup(textDetails) {
     let popup = new mapboxgl.Popup().setHTML(`<p>${textDetails}</p>`)
         .addTo(map);
 
     marker.setPopup(popup)
+}
+
+function getPopup(textdetails) {
+
+}
+
+function getReversedGeocode(point, marker) {
+    $.ajax({
+        url: `https://api.mapbox.com/geocoding/v5/mapbox.places/${point}.json?access_token=${mapboxgl.accessToken}`,
+        success: function (data) {
+            marker.SetPopup(getPopup(data.features[0].place_name));
+        }
+    })
 }
--- js/refactor-jquery.js
+++ js/refactor-jquery.js
@@ -1,19 +1,142 @@
-// //make sure the DOM is loaded first
-// document.addEventListener("DOMContentLoaded", function () {
+// ==========================  addEventListener()
+// (function(){
+//
+//     //get the target
+//     let header = document.querySelector("#override-bootstrap");
+//
+//     //define the listener
+//     let listener = function(e){
+//         console.log("event fired")
+//         header.style.backgroundColor = 'blue';
+//         header.querySelector("#main-title").innerText = "DOM DOM DOM DOMMMMMMMM";
+//         header.removeEventListener("click", listener);
+//     }
+//
+//     //bind the event, target, listener together
+//     header.addEventListener("click", listener);
+//
+// })()
+
+
+// ========================= Mouse events ===============================
+//
+
+// document.addEventListener('click', function(e){
+//     console.log(e.target);
+// });
+
+
+// $(document).hover(function(e){
+//     console.log(e.target);
+// }, function(e){
+//     console.log(e.target);
+// })
+
+// ************** Get an element and change style on click ****************
+
+// (function(){
+//
+//     //get the element
+//     let header = $('#override-bootstrap');
+//     //define a handler
+//     let handlerIn = function(){
+//         $(this).css('background-color', 'blue');
+//     }
+//
+//     let handlerOut = function(){
+//         $(this).removeAttr('style');
+//     }
+//     //bind the handler
+//     header.hover(handlerIn, handlerOut);
+//
+//
+// })()
+
+
+// ************** Get elements and change style on click *************
+
+// (function(){
+//
+//     let tableRows = [...$('tbody > tr')];
+//     let handlerBlueBGnd = function(e){
+//         //tableRows.css('background-color', '');
+//         let index = tableRows.indexOf(e.target);
+//         console.log(index);
+//         // if (index % 2 !== 0){
+//         //     $(this).css('background-color', 'lightblue');
+//         // }
+//     }
+//     //tableRows.click(handlerBlueBGnd);
+//
+//     for (let i = 0; i <= tableRows.length - 1; i++){
+//             let row = tableRows[i];
+//             row.addEventListener("click", handlerBlueBGnd);
+//     }
+//     // loop over the tableRows collection
+//     // for each element in the collection, bind a click event
+//
+// })()
+
+
+// ************* Change Table Row background colors on hover *********************
+// (function(){
+
+//  let jqClassObjs = $('.jqClass');
+//
+//  let handlerBlueBGnd = function(e){
+//      $(this).css({
+//          backgroundColor: 'lightblue',
+//          transition: 'background .25s'
+//      })
+//  };
+//
+//  let handlerNormalBGnd = function(e){
+//      $(this).css({
+//          backgroundColor: '',
+//          transition: 'background .25s'
+//      })
+//  }
+//
+// jqClassObjs.hover(handlerBlueBGnd, handlerNormalBGnd);
+
+// })();
+
+// ************ More dynamic background changes on click or hover *****************
+// (function () {
+//     let jqElements = $('.jqClass');
+//     let backgroundBlue = 'lightblue';
+//     let backgroundReset = '';
+//
+//     function addClickEvents(elementsToBind, color1, color2) {
+//         let handlerBlueBckGnd = function () {
+//             elementsToBind.css('background-color', color1);
+//             $(this)
+//                 .css('background-color', color2);
+//         }
+//         elementsToBind.click(handlerBlueBckGnd);
+//     }
+// //
+// //     addClickEvents(jqElements, backgroundReset, backgroundBlue);
+// //
+//     addClickEvents($(".card"), "", "red");
+
+
+//make sure the DOM is loaded first
+// document.addEventListener("DOMContentLoaded", function() {
 //     addListeners();
 // });
 //
 // //parent method for adding all listeners
-// function addListeners() {
+// function addListeners(){
 //     //get the elements to add listener and change image
-//     let cardContainers = Array.from(document.querySelectorAll(".col-md-3"));
-//     addEvents(cardContainers, 'images/jazz-music-rubber-duck.jpg', 'mouseover');
-//     addEvents(cardContainers, 'images/question.png', 'mouseout');
+//     let cardContainers = $(".col-md-3");
+//     addEvents(cardContainers, 'assets/jazz-music-rubber-duck.jpg', 'mouseover');
+//     addEvents(cardContainers, 'assets/300x300.png', 'mouseout');
 // }
 //
 // //add mouseover/mouseout events to all selected elements
-// function addEvents(cardContainers, imgPath, listenerType) {
-//     cardContainers.forEach((cc) => {
+// function addEvents(cardContainers, imgPath, listenerType){
+//     cardContainers.click((cc) => {
 //         let card = cc;
 //         //create listener which will call function change the image
 //         let listener = function (event) {
@@ -24,58 +147,226 @@
 //     });
 // }
 //
-//
-// // //actual function to swap images
-// // function changeImage(card, imgPath){
-// //     card.setAttribute('src', imgPath);
-// // }
-//
-// //
-// // $(".card").hover(
-// //     function () {
-// //         $(this).image("images/question.png");
-// //     },
-// //     function () {
-// //         $(this).text("card-text");
-// //     }
-// // );
-//
-// let keydownHandler= function(e){
-//     console.log(this.value);
-//     if(this.value === "juan"){
-//         alert("That's my name , dont wear it out!")
-//     }
+// //actual function to swap images
+// function changeImage(card, imgPath){
+//     card.setAttribute('src', imgPath);
 // }
-// $("input").keydown(keydownHandler)
-//
 
 
-///TODO: When the user clicks the button with the id of 'submitBtn,'
-// the background color of #override-bootstrap changes to 'red'//
+// =========================== FUN TIME! ================================
+//
+//     addListeners();
+//
+//     //parent method for adding all listeners
+//     function addListeners() {
+//         //get the elements to add listener and change image
+//         let cardContainers = $(".col-md-3");
+//         addEvents(cardContainers, 'assets/jazz-music-rubber-duck.jpg', 'assets/question.png');
+//     }
 
+//add mouseover/mouseout events to all selected elements
+// function addEvents(cardContainers, imgPath1, imgPath2) {
+//
+//     let handler1 = function (event) {
+//         let attrValue = $(this)
+//             .attr("data-attribute");
+//         $(this)
+//             .append(`<p id="attr-value">${attrValue}</p>`);
+//         $(this)
+//             .find(".card-img-top")
+//             .attr('src', imgPath1);
+//     };
+//
+//     let handler2 = function (event) {
+//         $(this)
+//             .find('#attr-value')
+//             .remove();
+//         $(this)
+//             .find(".card-img-top")
+//             .attr('src', imgPath2)
+//     };
+//
+//     cardContainers.hover(handler1, handler2);
+// }
+
+
+// ========================= Keyboard events
+
+
+// // ************************* keyup *************************
+
+
+// Array.from(document.getElementsByTagName('input'))
+//     .forEach((i) => {
+//         i.addEventListener('keyup', function(e){
+//             console.log(this.value);
+//             if (this.value === 'casey'){
+//                 alert("GOT IT!");
+//             }
+//     });
+// })
+
+// $(document).keyup(function(e){
+//     console.log(e.key);
+// })
+
+//     let keyupHandler = function (e) {
+//         console.log(this.value);
+//         if (this.value === 'casey') {
+//             alert("That's my name, don't wear it out!");
+//         }
+//     }
+//     $('input')
+//         .keyup(keyupHandler);
+//
+// })();
+
+// // ************************* keypress *************************
+//
+// (function(){
 //
 //
-// $(document).ready(function () {
-//     $('.card-text').click(function (){
-//         $(this).css("background-color", "red");
+//     Array.from(document.getElementsByTagName('input'))
+//         .forEach((i) => {
+//             i.addEventListener('keypress', function(e){
+//                 console.log(this.value);
+//                 if (this.value === 'casey'){
+//                     alert("GOT IT!");
+//                 }
+//         });
 //     })
-// });
+//
+//
+//    $('input').keypress(function(e){
+//        console.log(this.value);
+//        if (this.value === 'casey'){
+//            alert("That's my name, don't wear it out!");
+//        }
+//    });
+//
+// })()
 
-//TODO: When the user clicks a .card-text element,
-// change its background color to 'red
-// $(document).ready(function () {
-//     $('.card-text').click(function (){
-//         $(this).css("background-color", "red");
+// // ************************* keydown *************************
+
+// (function(){
+//
+//
+//     Array.from(document.getElementsByTagName('input'))
+//         .forEach((i) => {
+//             i.addEventListener('keydown', function(e){
+//                 console.log(this.value);
+//                 if (this.value === 'casey'){
+//                     alert("GOT IT!");
+//                 }
+//             });
+//         })
+//
+//
+//     $('input').keydown(function(e){
+//         console.log(this.value);
+//         if (this.value === 'casey'){
+//             alert("That's my name, don't wear it out!");
+//         }
+//     });
+//
+// })()
+
+// ============================ Remove Event Listener
+//
+// (function(){
+//         //get the target
+//         let submitBtn = document.querySelector('#submitBtn');
+//         console.log(submitBtn);
+//
+//
+//         let logger = function(){
+//             console.log("BAD NO BOOOOOO");
+//         }
+//
+//         let alertTheUser = function(){
+//             alert("You Did a Thing!");
+//             submitBtn.removeEventListener("click", logger);
+//         }
+//
+//         submitBtn.addEventListener("click", alertTheUser);
+//
+// })()
+
+
+// ******************** FUN TIME! ******************************
+// When I click on a card, the image should change to Zod or Superman
+// Only one card should be superman and he should only show up once
+// (function(){
+//
+//     //set the stage
+//     let cards = $('.card');
+//     let randomNumber = Math.floor(Math.random() * cards.length);
+//     let decremented = cards.length - 1;
+//
+//     //sanity check #1
+//     console.log("Random: " + randomNumber);
+//
+//     //do a thing to each card
+//     cards.each(function(){
+//
+//         //sanity check #2
+//         console.log("Decremented: " + decremented);
+//
+//         //make a new image path variable for each loop item
+//         let imgPath;
+//
+//         if (randomNumber === decremented){
+//             imgPath = 'img/superman.jpg';
+//         }else{
+//             imgPath = 'img/General_Zod_(circa_2018).png';
+//         }
+//
+//         let card = $(this);
+//         card.click(function(e){
+//             console.log("key pressed");
+//             card
+//                 .find('.card-img-top')
+//                 .attr('src', imgPath);
+//         });
+//
+//         decremented--;
 //     })
-// });
+// })()
 
-//TODO: Add a new button to the html, directly below the first button. Give it an id of 'resetBtn'.
-// When the user clicks that button, the page should reload. (HINT: BOM has a super easy method for reloading the page.
-// You'll still need a click event)
+// ============================ Input Exercise
 
+// TODO: Augment the event listener you placed on #submitBtn previously
+//  -> Now, grab the values from #first, #last, and #handlefield
+//  -> After making sure each is not null or empty, create a new <tr> and append a <td> for each input value + 1 for the "number"
+//      -> See the design and current html for guidance
+//  -> Add any classes that you see are in table row elements on the static HTML
+//  -> Append the new <tr> with all <td> elements to the DOM and check your results
 
-//TODO: When the user clicks the element with the id #main-title,
-//its text font size doubles
+// let submitListener = function () {
+//     let firstName = $("#first").val();
+//     let lastName = $("#last").val();
+//     let handle = $("#handleField").val();
+//
+//     if (firstName === null || firstName === ""){
+//         alert("Please fill in first name and resubmit");
+//         return;
+//     }
+//
+//     if (lastName === null || lastName === ""){
+//         alert("Please fill in last name and resubmit");
+//         return;
+//     }
+//
+//     if (handle === null || handle === ""){
+//         alert("Please fill in handle and resubmit");
+//         return;
+//     }
+//
+//     let tableRow = $("<tr></tr>");
+//     let tableData = $()
+// }
+//
+// $("#submitBtn").click(submitListener);
 
 
 // TODO: Add a data-attribute to each card container (.col-md-3)
@@ -83,28 +374,27 @@
 //  -> Now, when a user hovers over one of the .col-md-3 containers, you should replace the text inside #card-title with the data-attribute value
 //  -> When the user hovers out, the data-attribute value should be replaced with the original text ("Card")
 
-let hoverIn = function () {
-    let attributeValue = $(this).attr("data-attribute");
-    $(".card-title").text(attributeValue);
+// let hoverIn = function () {
+//     $(this)
+//         .find(".card-title")
+//         .text($(this)
+//             .attr("data-attribute"));
+// }
+//
+// let hoverOut = function () {
+//     $(this)
+//         .find(".card-title")
+//         .text("Card");
+// }
+//
+// $(".col-md-3")
+//     .hover(hoverIn, hoverOut);
 
-}
-
-let hoverOut = function () {
-    $(this).find(".card-title").text("Card")
-}
-
-
-$(".col-md-3").hover(hoverIn, hoverOut)
 
 // TODO: When an li with the .list-group-item class is clicked, replace the text with the same text, but uppercased
 //  -> ALSO, change the background of the li to a new color
 //  -> When the li is clicked again, reset the li background color to its original color
 //      -> Don't worry about changing the text back
-
-
-$('.list-group-item').click(function () {
-    $(this).css({"background-color": "red", "text-transform": "uppercase"})
-});
 
 // TODO: Change up the submit button event
 //  -> Add a new input with an id of #redirect-url
@@ -112,23 +402,45 @@ $('.list-group-item').click(function () {
 //  -> When the user clicks #submitBtn, redirect the page to the value of #redirect-url
 //  -> HINT: You can either add a new input or change the id of an existing input element to #redirect-url to save time
 
+$("#submitBtn").click(function(){
+
+    let url = $("#redirect-url").val();
+
+    if (!url.includes('https')){
+        url = 'https://' + url
+    }
+    location.href = url;
+})
 
 // TODO: After a 2 second delay (BOM), when the user loads the page, change #main-title to a value of your choosing (change text, background color, what have you)!
 
 
-// TODO: When the user clicks #submitBtn, log to the console the values of #first, #last, and #handleField
-//  -> If any of the fields are empty, alert the user to fill the empty control (be sure to tell them which control was empty)
 
 
-$("#submitBtn").click(function() {
 
-        console.log( $("#first").val());
 
-        console.log($("#last").val());
 
-        console.log($("#handleField").val());
-    }
-)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
